c5e703dd0bc7bcbcf65b2a21d4f42728c63a8773
malamba <lavhengwamalamba@gmail.com>
2024-04-17 00:04:10 +0200

    [UPDATED] Readfile function in OS1Assignment.java to return array

--- OS1Assignment.java
+++ OS1Assignment.java
@@ -1,25 +1,39 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class OS1Assignment {
-    public static String readfile(String filename) throws IOException {
-        String referenceString = "";
+    
+    public static ArrayList<String> readfile(String filename) throws IOException {
+        ArrayList<String> arrayList = new ArrayList<>() ;
         try {
             FileInputStream fileInputStream = new FileInputStream(filename);
             long byt;
-            while ((byt = fileInputStream.read()) != -1) {
-                referenceString += byt;
+            int remaining = fileInputStream.available();
+            for (int j = 0; j < (remaining/8);j++) {
+                String referenceString = "";
+                for (int i = 0 ; i <8 ; i++){
+                    byt = fileInputStream.read();
+                    referenceString = "" + byt + referenceString;
+                }
+                arrayList.add(referenceString);
+                // break;
             }
             fileInputStream.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return referenceString;
+        return arrayList;
+    }
+
+    public static String Hex_to_binary(String HexString){
+
+        return null;
     }
 
     public static void main(String[] args) throws IOException {
-        readfile("OS1testsequence");
+        System.out.println(readfile("OS1testsequence"));
     }
 
 }
