4a74c6c275925726e0aa4239536b22b177a0f3e5
malamba <lavhengwamalamba@gmail.com>
2024-04-17 00:46:04 +0200

    [ADDED] Hashmap and fillmap function

--- OS1Assignment.java
+++ OS1Assignment.java
@@ -2,8 +2,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class OS1Assignment {
+    static HashMap<String, String> indecies = new HashMap<>();
 
     public static ArrayList<String> readfile(String filename) throws IOException {
         ArrayList<String> arrayList = new ArrayList<>();
@@ -33,7 +35,26 @@ public class OS1Assignment {
         return binaryValue;
     }
 
+    public static String binary_to_new(String binaryValue) {
+        // Slice the 64bit string into 12 bits
+        String bit5 = binaryValue.substring(52, 57);
+        String bit7 = binaryValue.substring(57);
+
+        return null;
+    }
+
+    public static void fillmap() {
+        indecies.put("00000", "00010");
+        indecies.put("00001", "00100");
+        indecies.put("00010", "00001");
+        indecies.put("00011", "00111");
+        indecies.put("00100", "00011");
+        indecies.put("00101", "00101");
+        indecies.put("00110", "00110");
+    }
+
     public static void main(String[] args) throws IOException {
+        fillmap();
         ArrayList<String> arrayList = readfile("OS1testsequence");
         System.out.println(arrayList);
         System.out.println(Hex_to_binary(arrayList.get(0)));
